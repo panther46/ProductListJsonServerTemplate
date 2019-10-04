ba4093256a1689d893a6872b5b07197cb78b6855
Samir M R <samir@MacBook-Pro.home>
2019-10-04 16:19:39 +0200

    Version 1.05 Building Links for nav using Routing

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom'; //indispensable para crear links usando routing
+import {Link} from 'react-router-dom'; //indispensable para crear links usando routing, esto compila e imprime links convencionales.
 
 
 function Header(){
@@ -7,8 +7,15 @@ function Header(){
         <nav className = "navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
                 <Link to = "/products" className= "navbar-brand">
- 
+                    Json server and Routing
                 </Link>
+
+                <ul className= "navbar-nav mr-auto">
+                    <li className= "nav-item"></li>
+                        <Link to = '/products' className= "nav-link">Products</Link>
+                        <Link to = '/new-Product' className= "nav-link">Add new Product</Link>
+
+                </ul>
                 
             </div>
         </nav>
